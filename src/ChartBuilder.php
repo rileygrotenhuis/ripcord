6b9d0b3ce23148be6a150efba7126653b6b42cde
Riley Grotenhuis <rg@buildmidwestern.com>
2024-03-18 11:37:27 -0500

    feat: add polar area chart

--- src/ChartBuilder.php
+++ src/ChartBuilder.php
@@ -6,6 +6,7 @@ use Rileygrotenhuis\Ripcord\Charts\BarChart;
 use Rileygrotenhuis\Ripcord\Charts\BubbleChart;
 use Rileygrotenhuis\Ripcord\Charts\LineChart;
 use Rileygrotenhuis\Ripcord\Charts\PieChart;
+use Rileygrotenhuis\Ripcord\Charts\PolarAreaChart;
 use Rileygrotenhuis\Ripcord\Charts\ScatterChart;
 
 class ChartBuilder
@@ -39,5 +40,12 @@ class ChartBuilder
     public static function bubbleChart(array $data): BubbleChart
     {
         return new BubbleChart($data);
+    }
+
+    public static function polarAreaChart(
+        array $labels,
+        array $data
+    ): PolarAreaChart {
+        return new PolarAreaChart($labels, $data);
     }
 }
---
+++ src/Charts/PolarAreaChart.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Rileygrotenhuis\Ripcord\Charts;
+
+use Rileygrotenhuis\Ripcord\Colors;
+
+class PolarAreaChart extends Chart
+{
+    public function __construct(
+        public array $labels,
+        public array $data
+    ) {
+    }
+
+    public function build(): array
+    {
+        $datasets = array_map(function ($data) {
+            return [
+                'label' => $data['label'],
+                'data' => $data['data'],
+                'backgroundColor' => Colors::BACKGROUND_COLORS,
+            ];
+        }, $this->data);
+
+        return [
+            'labels' => $this->labels,
+            'datasets' => $datasets,
+        ];
+    }
+
+    public function validate(): bool
+    {
+        return $this->validateLabels() && $this->validateData();
+    }
+
+    protected function validateLabels(): bool
+    {
+        foreach ($this->labels as $label) {
+            if (! is_string($label)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected function validateData(): bool
+    {
+        foreach ($this->data as $chartData) {
+            if (! is_string($chartData['label'])) {
+                return false;
+            }
+
+            if (! is_array($chartData['data'])) {
+                return false;
+            }
+
+            foreach ($chartData['data'] as $data) {
+                if (! is_numeric($data)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
